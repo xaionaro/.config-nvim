4417fa5e517cac2a6cf4b2fa4b343b3396d297ab
xaionaro@dx.center <Dmitrii Okunev>
2026-02-02 19:22:29 +0000

    update mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,9 @@
 require "nvchad.mappings"
 
+-- Clear NvChad's default <C-c> mapping (copy whole file) to avoid conflict with buffer closing
+pcall(vim.keymap.del, "n", "<C-c>")
+
 local map = vim.keymap.set
 
 map("n", "<C-p>", "<cmd>Telescope frecency<CR>", { desc = "Telescope Frecency" })
+map("n", "<S-f>", "<cmd>Telescope live_grep<CR>", { desc = "Search by content (VS Code style)" })
