6bfa4ce86f6a87466f9ac439748dbadc2d521786
Dmitrii Okunev <xaionaro@dx.center>
2026-02-07 20:43:29 +0000

    Auto-reload files

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,15 @@
 require "nvchad.autocmds"
+
+-- Auto-reload files when changed externally (VSCode-like behavior)
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  desc = "Auto-reload files when changed externally",
+  command = "if mode() != 'c' | checktime | endif",
+})
+
+-- Notify when file is reloaded
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  desc = "Notify when file is reloaded",
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.INFO)
+  end,
+})
--- lua/options.lua
+++ lua/options.lua
@@ -3,3 +3,6 @@ require("custom").setup()
 
 vim.opt.title = true
 vim.opt.titlestring = "neovim - %t"
+
+-- Auto-reload files when changed externally (VSCode-like behavior)
+vim.opt.autoread = true
